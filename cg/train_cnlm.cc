32ab613988b7f8f8f9184fd8fa67b03ebe7cb918
Ed Grefenstette <egrefen@gmail.com>
2013-10-25 12:38:02 +0100

    User can now specify whether EOS symbols are added to source sentences during training

--- cg/train_cnlm.cc
+++ cg/train_cnlm.cc
@@ -22,7 +22,7 @@
 #include "cg/cnlm.h"
 #include "corpus/corpus.h"
 
-static const char *REVISION = "$Rev: 247 $";
+static const char *REVISION = "$Rev: 248 $";
 
 // Namespaces
 using namespace boost;
@@ -94,8 +94,9 @@ int main(int argc, char **argv) {
         "file containing word to class mappings in the format <class> <word> <frequence>.")
     ("window", value<int>()->default_value(-1), 
         "Width of window of source words conditioned on.")
+    ("no-source-eos", "do not add end of sentence tag to source representations.")
     ("verbose,v", "print perplexity for each sentence (1) or input token (2) ")
-    ("randomise", "visit the training tokens in random order")
+    ("randomise", "visit the training tokens in random order.")
     ("diagonal-contexts", "Use diagonal context matrices (usually faster).")
     ("non-linear", "use a non-linear hidden layer.")
     ;
@@ -130,6 +131,7 @@ int main(int argc, char **argv) {
   config.diagonal = vm.count("diagonal-contexts");
   config.nonlinear= vm.count("non-linear");
   config.source_window_width = vm["window"].as<int>();
+  config.source_eos = !vm.count("window");
 
   cerr << "################################" << endl;
   cerr << "# Config Summary:" << endl;
@@ -147,6 +149,7 @@ int main(int argc, char **argv) {
   cerr << "# diagonal = " << config.diagonal << endl;
   cerr << "# non-linear = " << config.nonlinear << endl;
   cerr << "# width = " << config.source_window_width << endl;
+  cerr << "# source-eos = " << config.source_eos << endl;
   cerr << "################################" << endl << endl;
 
   omp_set_num_threads(config.threads);
@@ -189,7 +192,8 @@ void learn(const variables_map& vm, ModelData& config) {
     Sentence& s = target_corpus.back();
     while (line_stream >> token) 
       s.push_back(target_dict.Convert(token));
-    s.push_back(end_id);
+    if (config.source_eos)
+            s.push_back(end_id);
     num_training_instances += s.size();
   }
   target_in.close();
