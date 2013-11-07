bd5cfb5fe60dc384f01a62184b02e883676814cc
Karl Moritz Hermann <mail@karlmoritz.com>
2013-11-07 17:37:33 +0000

    Fixed scripts

--- scripts/kmh/europarl-split.sh
+++ scripts/kmh/europarl-split.sh
@@ -5,8 +5,8 @@ echo $PWD
 for term in "euro.train.1k" "euro.dev.full"
 do
   python ../jointPreformatter.py \
-    ${EURODIR}/code/master/euro/${term}.en.txt \
-    ${EURODIR}/code/master/euro/${term}.de.txt \
+    ${EURODIR}/${term}.en.txt \
+    ${EURODIR}/${term}.de.txt \
     -o ${PWD} \
     -s ${term}_source \
     -t ${term}_target
