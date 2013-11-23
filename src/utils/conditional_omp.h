c81c9792857aaaf3e25a09cda64f7c58e8ff19fd
Phil Blunsom <Phil.Blunsom@cs.ox.ac.uk>
2013-11-23 12:10:00 +0000

    Added conditional inclusion of omp.h

--- src/utils/conditional_omp.h
+++ src/utils/conditional_omp.h
@@ -3,6 +3,7 @@
 #else
   typedef int omp_int_t;
   inline void omp_set_num_threads(int num_threads) {}
-  inline omp_int_t omp_get_thread_num() { return 0;}
-  inline omp_int_t omp_get_max_threads() { return 1;}
+  inline omp_int_t omp_get_thread_num()  { return 0; }
+  inline omp_int_t omp_get_num_threads() { return 1; }
+  inline omp_int_t omp_get_max_threads() { return 1; }
 #endif
