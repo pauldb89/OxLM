0e55fb1cffcfe2f418c5d0d36da1a3bb76a1cd83
Paul Baltescu <pauldb89@gmail.com>
2014-03-12 01:30:44 +0000

    Add unit tests for util functions.

--- src/lbl/tests/utils_test.cc
+++ src/lbl/tests/utils_test.cc
@@ -5,6 +5,24 @@
 
 namespace oxlm {
 
+TEST(TestSoftMax, BasicTest) {
+  VectorReal v(3);
+  v << 1, 2, 3;
+  VectorReal w = softMax(v);
+  VectorReal expected_w(3);
+  expected_w << 0.09003, 0.244728, 0.665240;
+  EXPECT_MATRIX_NEAR(expected_w, w, 1e-5);
+}
+
+TEST(TestLogSoftMax, BasicTest) {
+  VectorReal v(3);
+  v << 1, 2, 3;
+  VectorReal w = logSoftMax(v);
+  VectorReal expected_w(3);
+  expected_w << -2.407605, -1.407605, -0.407605;
+  EXPECT_MATRIX_NEAR(expected_w, w, 1e-5);
+}
+
 TEST(TestSigmoid, BasicTest) {
   VectorReal v(3);
   v << -1, 0, 1;
