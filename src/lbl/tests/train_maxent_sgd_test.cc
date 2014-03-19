ab86b957885e0860f7002526225d2fffec04e6dd
Paul Baltescu <pauldb89@gmail.com>
2014-03-19 12:41:10 +0000

    Integration test for sparse features.

--- src/lbl/tests/train_maxent_sgd_test.cc
+++ src/lbl/tests/train_maxent_sgd_test.cc
@@ -14,4 +14,14 @@ TEST_F(TestSGD, TestTrainMaxentSGD) {
   EXPECT_NEAR(60.1676, exp(-log_pp / test_corpus.size()), 1e-3);
 }
 
+TEST_F(TestSGD, TestTrainMaxentSGDSparseFeatures) {
+  config.feature_context_size = 3;
+  config.sparse_features = true;
+  FactoredMaxentNLM model = learn(config);
+  config.test_file = "test.txt";
+  Corpus test_corpus = loadTestCorpus(model.label_set());
+  double log_pp = perplexity(model, test_corpus);
+  EXPECT_NEAR(64.0444, exp(-log_pp / test_corpus.size()), 1e-3);
+}
+
 } // namespace oxlm
