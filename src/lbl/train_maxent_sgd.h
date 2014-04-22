be72629144d1af8e624c4ddd245fc7d66b6f11e1
Paul Baltescu <pauldb89@gmail.com>
2014-04-22 18:39:43 +0100

    Remove debug statements.

--- src/lbl/train_maxent_sgd.h
+++ src/lbl/train_maxent_sgd.h
@@ -293,9 +293,6 @@ FactoredMaxentNLM learn(ModelData& config) {
               global_gradientU, global_gradientV, minibatch_indices);
         }
 
-        #pragma omp master
-        cout << "done minibatch init: " << GetDuration(iteration_start, GetTime()) << " seconds..." << endl;
-
         // Wait for global gradients to be cleared before updating them with
         // data from this minibatch.
         #pragma omp barrier
@@ -311,9 +308,6 @@ FactoredMaxentNLM learn(ModelData& config) {
             model, training_corpus, minibatch_thread_indices, index, extractor,
             g_R, g_Q, g_C, g_B, g_F, g_FB, g_U, g_V);
 
-        #pragma omp master
-        cout << "done sgd gradient: " << GetDuration(iteration_start, GetTime()) << " seconds..." << endl;
-
         #pragma omp critical
         {
           global_gradient += gradient;
@@ -325,9 +319,6 @@ FactoredMaxentNLM learn(ModelData& config) {
           }
           av_f += f;
         }
-
-        #pragma omp master
-        cout << "done sgd update: " << GetDuration(iteration_start, GetTime()) << " seconds..." << endl;
 
         // All global gradient updates must be completed before executing
         // adagrad updates.
@@ -342,8 +333,6 @@ FactoredMaxentNLM learn(ModelData& config) {
             adaGradV[i]->updateSquared(global_gradientV[i]);
           }
 
-          cout << "done squared update: " << GetDuration(iteration_start, GetTime()) << " seconds..." << endl;
-
           model.W -= global_gradient.binaryExpr(
               adaGrad, CwiseAdagradUpdateOp<Real>(step_size));
           model.F -= global_gradientF.binaryExpr(
@@ -356,8 +345,6 @@ FactoredMaxentNLM learn(ModelData& config) {
             model.V[i]->updateAdaGrad(global_gradientV[i], adaGradV[i], step_size);
           }
 
-          cout << "done adagrad update: " << GetDuration(iteration_start, GetTime()) << " seconds..." << endl;
-
           // regularisation
           if (config.l2_lbl > 0 || config.l2_maxent > 0) {
             Real minibatch_factor = static_cast<Real>(end - start) / training_corpus->size();
@@ -366,12 +353,6 @@ FactoredMaxentNLM learn(ModelData& config) {
             av_f += model.l2Objective(
                 global_gradientU, global_gradientV, minibatch_factor);
           }
-        }
-
-        #pragma omp master
-        {
-          cout << "done all updates: " << GetDuration(iteration_start, GetTime()) << " seconds..." << endl;
-          cout << endl;
         }
 
         // Wait for master thread to update model.
