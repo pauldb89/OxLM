1d2adbfaa691e2493dc194ec48380f4104ff13e1
Paul Baltescu <pauldb89@gmail.com>
2014-05-11 14:11:15 +0100

    Finally fix git revision issue.

--- cmake/GitRevision.cmake
+++
@@ -1,4 +0,0 @@
-execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
-  OUTPUT_VARIABLE GIT_REVISION OUTPUT_STRIP_TRAILING_WHITESPACE)
-file(WRITE ../src/utils/git_revision.h
-  "const char* GIT_REVISION = \"${GIT_REVISION}\";")
