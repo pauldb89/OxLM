bfb4c8ddb13e0190d5e0655e1abaadb7e0d85b18
armatthews <armatthe@cmu.edu>
2014-09-20 23:53:42 -0400

    Added comments to cdec converter helper classes

--- src/lbl/cdec_rule_converter.cc
+++ src/lbl/cdec_rule_converter.cc
@@ -7,10 +7,19 @@ CdecRuleConverter::CdecRuleConverter(
     const boost::shared_ptr<CdecStateConverter>& state_converter)
     : mapper(mapper), stateConverter(state_converter) {}
 
+// target is the target side of a hyperedge rule, encoded using cdec's vocab.
+// prev_states will contain one state per non-terminal in target.
+// Returns an encoding of the hyperedge using OxLM's vocabulary.
 vector<int> CdecRuleConverter::convertTargetSide(
     const vector<int>& target, const vector<const void*>& prev_states) const {
   vector<int> ret;
   for (int symbol: target) {
+    // Non-terminals are indexed starting at 0 and counting down
+    // X_1 = 0, X_2 = -1, X_3 = -2, ...
+
+    // If we see a terminal, map it from cdec's vocab to OxLM s vocab
+    // If we have a non-terminal, it will have a corresponding element in prev_states.
+    // Each state is just a sequence of symbols, so we load them and add them to the output. 
     if (symbol <= 0) {
       const auto& symbols = stateConverter->convert(prev_states[-symbol]);
       ret.insert(ret.end(), symbols.begin(), symbols.end());
--- src/lbl/cdec_state_converter.cc
+++ src/lbl/cdec_state_converter.cc
@@ -2,9 +2,27 @@
 
 namespace oxlm {
 
+/*
+These functions convert between a vector of ints and a void* "state".
+The void* points to an int array followed by a single character.
+The single character is at offset state_offset, and indicates
+how many of the integers in the array are actually used. Any
+remaining integers are garbage.
+
+For example if the maximum state size were 4, but we wanted to
+only store a vector of size 2, we would write it as:
+
+bytes 0-4: first integer
+bytes 5-8: second integer
+bytes 9-12: garbage
+bytes 13-16: garbage
+byte 17: size of vector, in this case 2
+*/
+
 CdecStateConverter::CdecStateConverter(int state_offset)
     : stateOffset(state_offset) {}
 
+// convert a state to a vector of ints
 vector<int> CdecStateConverter::convert(const void* state) const {
   int state_size = getStateSize(state);
   const int* buffer = reinterpret_cast<const int*>(state);
@@ -15,6 +33,8 @@ vector<int> CdecStateConverter::convert(const void* state) const {
   return symbols;
 }
 
+// convert an array of symbols into a state
+// state is an output parameter.
 void CdecStateConverter::convert(
     const vector<int>& symbols, void* state) const {
   int* new_state = reinterpret_cast<int*>(state);
